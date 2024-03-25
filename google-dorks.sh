e5752af38281357aa90581cc0dd7c3fc1d45bac8
ypn007 <55645320+ypn007@users.noreply.github.com>
2024-03-25 22:28:13 +0630

    Create google-dorks.sh

---
+++ google-dorks.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+echo -e "
+         \e[1;33m   ********************************************
+            *                                          *
+            *             Google Dorking Tool          *
+            *           Coded By :: CyberBullet        *
+            *        Hack Everything,Harm Nothing      *
+            *                                          *
+            ********************************************
+\e[0m"
+
+if [[ $# != 2 ]];then
+echo -e "\e[1;31mPlease run the script as bash $0 <username> <password>\e[0m"
+exit
+else
+code=$(curl -Iks -u $1:$2 https://google-reverse-proxy.duckdns.org/ | head -1 | awk '{ print $2 }')
+if [[ $code != 200 ]];then
+echo -e "\e[1;31mAuthentication failed\e[0m"
+exit
+else
+n=1
+read -p $'\e[1;32mEnter dork :: \e[0m' dork
+for i in {1..1000..10};do
+for urls in `curl -u $1:$2 -ks "https://google-reverse-proxy.duckdns.org/search?q=$dork&start=$i" -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0' | grep -Eoi '<a[^>]+>' | grep 'UWckNb' | grep -oP 'href="\K[^"]+'`;do
+echo -e "\e[1;32m [\e[1;31m$n\e[1;32m] \e[1;33m$urls\e[0m"
+n=$((n+1))
+done
+done
+fi
+fi
